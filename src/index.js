d77e0563fc101dd8edfedd33f6186ecb30a0b71c
geoek <jerome.maruejouls@gmail.com>
2021-06-09 22:34:54 +0200

    add geojson layer in data

--- src/index.js
+++ src/index.js
@@ -1,22 +1,34 @@
 import 'ol/ol.css'
-import {Map, View,Feature,Overlay} from 'ol';
+import 'ol-layerswitcher/dist/ol-layerswitcher.css'
+import {Map, View,Feature,Overlay} from 'ol'
 import {osmLayer, stamenLayer} from './layersWms.js'
 import {Group} from 'ol/layer'
 import {Point} from 'ol/geom'
 import {Vector as LayerVector} from 'ol/layer'
 import {Vector as SourceVector} from 'ol/source'
 import {Draw, Select, Modify} from 'ol/interaction'
-import {Circle, Fill, Stroke, Style} from 'ol/style';
-import GeometryType from 'ol/geom/GeometryType';
+import {Circle, Fill, Stroke, Style} from 'ol/style'
+import GeometryType from 'ol/geom/GeometryType'
 import Poi from './poi.js'
-import GeoJSON from 'ol/format/GeoJSON';
-import VectorSource from 'ol/source/Vector';
+import GeoJSON from 'ol/format/GeoJSON'
+import VectorSource from 'ol/source/Vector'
+import proj4 from 'proj4'
+import {register} from 'ol/proj/proj4'
+import {get as olProjGet} from 'ol/proj'
+import {transform} from 'ol/proj'
+import LayerSwitcher from 'ol-layerswitcher'
+
+// ajout de la projection 2154
+proj4.defs('EPSG:2154',"+proj=lcc +lat_1=49 +lat_2=44 +lat_0=46.5 +lon_0=3 +x_0=700000 +y_0=6600000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs");
+// ensure OL knows about the PROJ4 definitions
+register(proj4);
 
 
 let baselayers = new Group({
 	'title': 'Base Maps',
 	layers: [stamenLayer,osmLayer]
-});
+})
+
 
 const map = new Map({
 /*	controls: defaultControls().extend([
@@ -27,12 +39,19 @@ const map = new Map({
 	layers: [baselayers],
 	target: document.getElementById('map'),
 	view: new View({
-		//projection: 'EPSG:2154',
+		projection: 'EPSG:3857',
 		center: [300000,5950000],
 		zoom: 6,
 		//extent: [-2500000, 3600000, 3100000, 8000000],  
 	}),
-});
+})
+
+var layerSwitcher = new LayerSwitcher({
+	reverse: true,
+	activationMode: 'click',
+	groupSelectStyle: 'group'
+  });
+map.addControl(layerSwitcher);
 
 const myFirstPoi = new Poi({
 	name: 'Poi1',
@@ -40,8 +59,9 @@ const myFirstPoi = new Poi({
 	coord: "34.2 23.5"
 })
 
-var coord = [300000, 5950000];
+var coord = [300000, 5950000]
 var layer = new LayerVector({
+	title: "My point",
 	source: new SourceVector({
 		features: [
 			new Feature({
@@ -63,7 +83,53 @@ var layer = new LayerVector({
 	})
 });
 
-map.addLayer(layer);
+map.addLayer(layer)
+
+
+var localGeoLayer = new LayerVector({
+	title: "My json",
+	source: new SourceVector({
+		url: 'data/mygeojson.geojson',
+		format: new GeoJSON(),
+	}),
+	style: new Style({
+		image: new Circle({
+			radius: 10,
+			fill: new Fill({
+				color: '#3399CC',
+			}),
+			stroke: new Stroke({
+				color: '#fff',
+				width: 0.5,
+			}),
+		}),
+	})
+});
+
+map.addLayer(localGeoLayer)
+
+
+/*
+new LayerVector({
+	source: new GeoJSON({
+	   projection : 'EPSG:3857',
+	   url: 'data/mygeojson.geojson'
+	}),
+	style: new Style({
+		image: new Circle({
+			radius: 10,
+			fill: new Fill({
+				color: '#0000FF',
+			}),
+			stroke: new Stroke({
+				color: '#fff',
+				width: 0.5,
+			}),
+		}),
+	})
+})
+*/
+
 
 
 ////////////////////////////
@@ -103,28 +169,30 @@ map.on('singleclick', function (event) {
 */
 
 /////////////////////////////////////////////////////////////////////
-// Affichage des buffers depuis Postgis avec update de la position //
+// Creation de la couche des POI lié à une donnée GeoJson          //
 /////////////////////////////////////////////////////////////////////
-var geojsonObject = {
-    'type': 'FeatureCollection',
-    'crs': {
-        'type': 'name'
-        },
-        'features':[]
+
+var poiGeojsonObject = {
+    'features':[],
+	'type': 'FeatureCollection',
+	'name': 'mygeojson',
+	"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:EPSG::3857" } },
+
 };
 
-var features = (new GeoJSON()).readFeatures(geojsonObject);
-var source = new VectorSource({
-	features: features,
+var poiFeatures = (new GeoJSON()).readFeatures(poiGeojsonObject);
+console.log(poiFeatures)
+var poiSource = new VectorSource({
+	features: poiFeatures,
 	format: new GeoJSON()
 });
-
-var vectorLayer = new LayerVector({
-	title: "Vector",
-	source: source,
+console.log(poiSource)
+var poiLayer = new LayerVector({
+	title: "My POI",
+	source: poiSource,
 	style: new Style({
 		image: new Circle({
-			radius: 10,
+			radius: 5,
 			fill: new Fill({
 				color: '#FF0000',
 			}),
@@ -136,11 +204,15 @@ var vectorLayer = new LayerVector({
 	})
 });
 
-map.addLayer(vectorLayer);
+map.addLayer(poiLayer)
 
+
+/////////////////////////////////////////////////////////////////////
+// Creation des interactions avec la carte                         //
+/////////////////////////////////////////////////////////////////////
 
 var draw
-var typeSelect = document.getElementById('type');
+var typeInterraction = document.getElementById('typeAction')
 
 var select = new Select();
 var modify = new Modify({
@@ -148,40 +220,54 @@ var modify = new Modify({
 });
 
 function addInteraction(){
-	var value = typeSelect.value;
+	var value = typeInterraction.value
 	if (value !== 'None') {
 		draw = new Draw({
-		source: source,
-		type: ("Point")       
+			source: poiSource,
+			type: ("Point")       
 		});
-		map.addInteraction(draw);
-		source.addFeatures(draw);
-		console.log(source)
-		}
-	else{
-		map.addInteraction(select);
-		map.addInteraction(modify);
-		}
-
+		map.addInteraction(draw)
+		poiSource.addFeatures(draw)
+		console.log(poiSource)
+		debugGeoJson()
 	}
+	else {
+		map.addInteraction(select)
+		map.addInteraction(modify)
+	}
+}
 
-
-
-typeSelect.onchange = function() {
+typeInterraction.onchange = function() {
 	map.removeInteraction(draw);
-	map.removeInteraction(select);
-	map.removeInteraction(modify);
-	addInteraction();
+	map.removeInteraction(select)
+	map.removeInteraction(modify)
+	addInteraction()
 };
 
-addInteraction();
+addInteraction()
 
-var writer = new GeoJSON();
-var geojsonStr = writer.writeFeatures(source.getFeatures());
 
-function getfeatures(){
-document.getElementById("demo").innerHTML = geojsonStr;
+/////////////////////////////////////////////////////////////////////
+// Export de la donnée GeoJson                                     //
+/////////////////////////////////////////////////////////////////////
+
+function debugGeoJson() {
+	var writer = new GeoJSON()
+	var geojsonStr = writer.writeFeatures(poiSource.getFeatures())
+	console.log(poiFeatures)
+	//var geojsonStr = writer.writeFeatures(transform(poiSource.getFeatures(),'EPSG:4326', 'EPSG:3857'))
+
+	document.getElementById("demo").innerHTML = geojsonStr
 }
+
+map.on("singleclick", function(evt){
+	debugGeoJson()
+})
+
+
+
+
+
 /*
 map.on("singleclick", function(evt){
 	// récupération des coordonnées
